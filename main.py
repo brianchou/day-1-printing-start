a02fe2eced59621c0956b3807b358cef0ef88dcd
Brian Chou <brianchou_5@hotmail.com>
2022-03-25 12:33:28 +0000

    Done for coding

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 # Write your code below this line 👇
-print(2**3)
+bandFirstName = input("What's name of the city you grew up in?\n")
+bandLastName = input("What's your pet's name?\n")
+print("Your band name could be " + bandFirstName + " " + bandLastName +".")
